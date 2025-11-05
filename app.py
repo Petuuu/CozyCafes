1ed4e477e6692f8e399f3ca2c3b55c87df9344b6
Petu <pojp07@gmail.com>
2025-11-05 11:17:51 +0200

    feat(app): add basic reviews (only comment), no database yet

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
+reviews = []
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", reviews=reviews)
 
 
-app.run(debug=True)
+@app.route("/review")
+def review():
+    return render_template("review.html")
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    reviews.append(request.form["comment"])
+    return redirect(url_for("index"))
