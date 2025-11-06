c8b893afa8ff6e7f0791cee08adc434fb5024e51
Petu <pojp07@gmail.com>
2025-11-07 01:48:36 +0200

    refactor(app): create module for interacting with database

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, request, render_template, redirect
-import sqlite3
+import db
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    db = sqlite3.connect("database.db")
-    reviews = db.execute("SELECT comment FROM Reviews").fetchall()
-    db.close()
-    return render_template("index.html", reviews=reviews)
+    r = db.execute("SELECT comment FROM Reviews")
+    return render_template("index.html", reviews=r)
 
 
 @app.route("/review")
@@ -20,9 +18,5 @@ def review():
 @app.route("/send", methods=["POST"])
 def result():
     comment = request.form["comment"]
-
-    db = sqlite3.connect("database.db")
     db.execute("INSERT INTO Reviews (comment) VALUES (?)", [comment])
-    db.commit()
-    db.close()
     return redirect("/")
---
+++ db.py
@@ -0,0 +1,28 @@
+import sqlite3
+from flask import g
+
+
+def get_connection():
+    db = sqlite3.connect("database.db")
+    db.execute("PRAGMA foreign_keys = ON")
+    db.row_factory = sqlite3.Row
+    return db
+
+
+def last_insert_id():
+    return g.last_insert_id
+
+
+def execute(query, params=[]):
+    db = get_connection()
+    r = db.execute(query, params)
+    db.commit()
+    g.last_insert_id = r.lastrowid
+    db.close()
+
+
+def query(query, params):
+    db = get_connection()
+    r = db.execute(query, params).fetchall()
+    db.close()
+    return r
