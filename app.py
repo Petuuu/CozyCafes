e4cdeff47d519301bd90d8a281983033950dec5f
Petu <pojp07@gmail.com>
2025-11-07 03:14:42 +0200

    feat(app): reviews can now be edited and deleted

--- app.py
+++ app.py
@@ -6,17 +6,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    r = db.query("SELECT comment FROM Reviews")
+    r = db.query("SELECT id, comment FROM Reviews")
     return render_template("index.html", reviews=r)
 
 
-@app.route("/review")
-def review():
-    return render_template("review.html")
+@app.route("/add_item", methods=["GET", "POST"])
+def add_item():
+    if request.method == "GET":
+        return render_template("review.html")
 
-
-@app.route("/send", methods=["POST"])
-def result():
     comment = request.form["comment"]
     db.execute("INSERT INTO Reviews (comment) VALUES (?)", [comment])
+    return redirect("/")
+
+
+@app.route("/edit_item/<int:id>", methods=["GET", "POST"])
+def edit_item(id):
+    if request.method == "GET":
+        r = db.query("SELECT id, comment FROM Reviews WHERE id = ?", [id])
+        return render_template("edit.html", review=r[0])
+
+    edit = request.form["comment"]
+    db.execute("UPDATE Reviews SET comment = ? where id = ?", [edit, id])
+    return redirect("/")
+
+
+@app.route("/delete_item/<int:review_id>")
+def delete_item(review_id):
+    db.execute("DELETE FROM Reviews WHERE id = ?", [review_id])
     return redirect("/")
